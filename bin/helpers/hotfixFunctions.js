89131c04ceddf91640403addb682fc92a9e3a8d7
Damian Cardozo <damian@kzsoftworks.com>
2022-01-20 00:33:17 -0300

    Create hotfixFunctions

---
+++ bin/helpers/hotfixFunctions.js
@@ -0,0 +1,28 @@
+const shell = require("shelljs");
+const { MoveToBranch } = require("./movers");
+const {GetLogsFromHotFixStart} = require("./gitFunctions")
+
+async function FinishHotFixByIdAndName({ jiraId, name, noDev }) {
+  console.log("\x1b[36m%s\x1b[0m", "OUTPUT:\n");
+  shell.exec(`git checkout master`);
+  shell.exec(`git merge --no-ff hotfix/CTDEV-${jiraId}_${name}`);
+
+  if (!noDev) {
+    MoveToBranch(`hotfix/CTDEV-${jiraId}_${name}`);
+    let { storyPoint } = await GetLogsFromHotFixStart(argv);
+    console.log(storyPoint);
+    shell.exec(`git rebase -i ${storyPoint}`);
+    storyPoint = await GetLogsFromHotFixStart(argv, true);
+    shell.exec(
+      "git checkout pepito/epic/CTDEV-69/CTDEV-69_hotFixesAndReleases"
+    );
+    shell.exec(`git cherry-pick ${storyPoint.storyPoint}`);
+    shell.exec(`git branch -d hotfix/CTDEV-${jiraId}_${name}`);
+  } else {
+    shell.exec(`git branch -d hotfix/CTDEV-${jiraId}_${name}`);
+  }
+}
+
+module.exports = {
+  FinishHotFixByIdAndName: FinishHotFixByIdAndName,
+};
